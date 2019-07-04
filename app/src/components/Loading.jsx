4447c62d28d5ef30667657cd7daa6fa642c9e80f
Anshuman Bhardwaj <anshu5074@gmail.com>
2019-07-04 18:18:33 +0530

    Add loading component

--- app/src/components/Loading.jsx
+++ app/src/components/Loading.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import '../App.css';
+import styled, { keyframes } from 'styled-components';
+
+const LoadingTag = styled.p`
+    margin: 0px;
+    padding: 0px;
+    position: ${props => props.bottom ? 'absolute' : 'relative'};
+    width: 100px;
+    bottom: ${props => props.bottom ? '10px' : '0px'};
+    left: ${props => props.bottom ? 'calc(50vw - 50px)' : '0px'};
+
+`;
 
 const Loading = ({ bottom }) => (
-    <p className={bottom ? 'loading-bottom' : 'loading'}>Loading ...</p>
+    <LoadingTag bottom={bottom}>Loading ...</LoadingTag>
 );
 
 export default Loading;
