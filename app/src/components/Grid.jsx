2904de05ae41f7aa9e4bc7d167708a98994acd7f
Anshuman Bhardwaj <anshu5074@gmail.com>
2019-07-04 18:18:25 +0530

    Implement Card container

---
+++ app/src/components/Grid.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    display: flex;
+    flex-direction: row;
+    align-content: flex-start;
+    padding: 10px 20px;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 40px;
+    flex-wrap: wrap;
+    min-height: 100vh;
+`;
