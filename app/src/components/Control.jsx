9e982c2095c4ce97572991639e26a9c2440a54d0
Anshuman Bhardwaj <anshu5074@gmail.com>
2019-07-04 20:21:17 +0530

    Add control components

---
+++ app/src/components/Control.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Select = styled.select`
+    width: 150px;
+    padding: 5px;
+    border: none;
+    font-size: 16px;
+`;
+
+export const Label = styled.label`
+    margin-right: 10px;
+`;
+
+export const Option = styled.option`
+    height: 60px;
+    padding: 10px;
+`;
