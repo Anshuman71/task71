563bd82f7a024894a8b275b8265ba9c7a871bcfd
Anshuman Bhardwaj <anshu5074@gmail.com>
2019-07-05 13:42:54 +0530

    Add Infinite scroll

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
-import Loading from './components/Loading';
+import Loading, { EndOfList } from './components/Loading';
 import Card from './components/Card';
 import Grid from './components/Grid';
 import { Select, Option, Label } from './components/Control';
+import Header from './components/Header';
 
 const DATA_ENDPOINT = 'http://localhost:8000/api/products';
 
@@ -12,52 +13,69 @@ const LIMIT = 20;
 function App() {
     const [items, setItems] = useState([]);
     const [loading, toggleLoading] = useState(true);
-    const [buffer, setBuffer] = useState([]);
+    const [hasMore, setHasMore] = useState(true);
     const [sortBy, setSortBy] = useState('id');
-    const [pageNo, setPageNo] = useState(0);
+    const [pageNo, setPageNo] = useState(1);
 
     const fetchItems = useCallback(async () => {
-        const order = sortBy === 'date' ? 'desc' : 'asc';
-        const url = `${DATA_ENDPOINT}?_sort=${sortBy}&_order=${order}&_page=${pageNo}&_limit=${LIMIT}`;
+        const url = `${DATA_ENDPOINT}?_page=${pageNo}&_sort=${sortBy}&_limit=${LIMIT}`;
         toggleLoading(true);
         const result = await fetch(url);
         const products = await result.json();
-        setItems(products);
-        console.log({ url, products });
+        if (products.length < 20) {
+            setHasMore(false);
+        }
+        setItems(prevState => [...prevState, ...products]);
         toggleLoading(false);
-    }, [sortBy, pageNo]);
+    }, [pageNo, sortBy]);
+
+    const handleScroll = () => {
+        if (
+            window.scrollY + window.innerHeight ===
+                document.documentElement.offsetHeight &&
+            hasMore &&
+            !loading
+        ) {
+                setPageNo(pageNo + 1);
+        }
+    };
 
     const updateSort = event => {
         setSortBy(event.target.value);
     };
 
     useEffect(() => {
+        window.addEventListener('scroll', handleScroll, false);
+        return () => window.removeEventListener('scroll', handleScroll, false);
+    });
+
+    useEffect(() => {
         fetchItems();
-    }, [fetchItems, sortBy]);
+    }, [fetchItems]);
+
 
     return (
         <div className="App">
-            <h1>The Emoji Store</h1>
-            <Label>Sort By</Label>
-            <Select value={sortBy} onChange={updateSort}>
-                <Option value="price">Price</Option>
-                <Option value="size">Size</Option>
-                <Option selected value="id">
-                    Id
-                </Option>
-            </Select>
-            {loading ? (
-                <Loading />
-            ) : (
+            <Header>
+                <h1>The Emoji Store</h1>
+                <div>
+                    <Label>Sort By</Label>
+                    <Select defaultValue="id" onChange={updateSort}>
+                        <Option value="price">Price</Option>
+                        <Option value="size">Size</Option>
+                        <Option value="id">Id</Option>
+                    </Select>
+                </div>
+            </Header>
+            {loading && hasMore ? <Loading bottom /> : null}
+            {items.length ? (
                 <Grid>
-                    {items.length
-                        ? items.map(emoji => (
-                              <Card key={emoji.id} emoji={emoji} />
-                          ))
-                        : null}
-                    <Loading bottom />
+                    {items.map(emoji => (
+                        <Card key={emoji.id} emoji={emoji} />
+                    ))}
                 </Grid>
-            )}
+            ) : null}
+            {!hasMore ? <EndOfList /> : null}
         </div>
     );
 }
