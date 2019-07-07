ab5bf12a68fdacf332c831c05314fc0021f57602
Anshuman Bhardwaj <anshu5074@gmail.com>
2019-07-07 09:25:28 +0530

    Add animation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,26 +5,47 @@ const LoadingTag = styled.p`
     margin: 0px;
     padding: 0px;
     position: absolute;
-    width: 100px;
+    width: 120px;
     bottom: 10px;
-    left: calc(50vw - 50px);
+    font-size: 20px;
+    left: calc(50vw - 60px);
+`;
 
+const Loaders = styled.span`
+    animation: extend 1.5s steps(3, end) infinite;
+    display: inline-block;
+    overflow: hidden;
+    vertical-align: bottom;
+    &:before {
+        content: '...';
+    }
+
+    @keyframes extend {
+        0% {
+            width: 0.25em;
+        }
+        100% {
+            width: 1em;
+        }
+    }
 `;
 
 const Loading = () => (
-    <LoadingTag >Loading ...</LoadingTag>
+    <LoadingTag>
+        Loading
+        <Loaders />{' '}
+    </LoadingTag>
 );
-
 
 const Message = styled.div`
     margin: 0px;
     padding: 0px;
-    position:absolute; 
+    position: absolute;
     width: 150px;
     bottom: 10px;
     left: calc(50vw - 75px);
 `;
 
-export const EndOfList = () => <Message>~ end of catalogue ~</Message>
+export const EndOfList = () => <Message>~ end of catalogue ~</Message>;
 
 export default Loading;
